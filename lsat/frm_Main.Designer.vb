a78b8244a032db1ce83aef20d90861c7ddd97bf9
Jayden Wheeler <jwheeler@larkinstreetyouth.org>
2018-12-21 12:55:57 -0800

    Icon alignment

--- lsat/frm_Main.Designer.vb
+++ lsat/frm_Main.Designer.vb
@@ -38,7 +38,6 @@ Partial Class frm_Main
         Me.PictureBox7 = New System.Windows.Forms.PictureBox()
         Me.Label7 = New System.Windows.Forms.Label()
         Me.PictureBox8 = New System.Windows.Forms.PictureBox()
-        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
         Me.Label8 = New System.Windows.Forms.Label()
         CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
         CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).BeginInit()
@@ -64,7 +63,7 @@ Partial Class frm_Main
         '
         Me.Label1.AutoSize = True
         Me.Label1.Font = New System.Drawing.Font("Calibri", 16.2!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Italic), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label1.Location = New System.Drawing.Point(683, 55)
+        Me.Label1.Location = New System.Drawing.Point(700, 55)
         Me.Label1.Name = "Label1"
         Me.Label1.Size = New System.Drawing.Size(232, 35)
         Me.Label1.TabIndex = 1
@@ -83,7 +82,7 @@ Partial Class frm_Main
         'PictureBox3
         '
         Me.PictureBox3.Image = CType(resources.GetObject("PictureBox3.Image"), System.Drawing.Image)
-        Me.PictureBox3.Location = New System.Drawing.Point(38, 288)
+        Me.PictureBox3.Location = New System.Drawing.Point(38, 269)
         Me.PictureBox3.Name = "PictureBox3"
         Me.PictureBox3.Size = New System.Drawing.Size(64, 64)
         Me.PictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize
@@ -93,7 +92,7 @@ Partial Class frm_Main
         'Label2
         '
         Me.Label2.AutoSize = True
-        Me.Label2.Location = New System.Drawing.Point(193, 307)
+        Me.Label2.Location = New System.Drawing.Point(193, 293)
         Me.Label2.Name = "Label2"
         Me.Label2.Size = New System.Drawing.Size(115, 17)
         Me.Label2.TabIndex = 4
@@ -102,7 +101,7 @@ Partial Class frm_Main
         'PictureBox4
         '
         Me.PictureBox4.Image = CType(resources.GetObject("PictureBox4.Image"), System.Drawing.Image)
-        Me.PictureBox4.Location = New System.Drawing.Point(38, 400)
+        Me.PictureBox4.Location = New System.Drawing.Point(38, 383)
         Me.PictureBox4.Name = "PictureBox4"
         Me.PictureBox4.Size = New System.Drawing.Size(64, 64)
         Me.PictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize
@@ -112,7 +111,7 @@ Partial Class frm_Main
         'Label3
         '
         Me.Label3.AutoSize = True
-        Me.Label3.Location = New System.Drawing.Point(196, 422)
+        Me.Label3.Location = New System.Drawing.Point(193, 407)
         Me.Label3.Name = "Label3"
         Me.Label3.Size = New System.Drawing.Size(91, 17)
         Me.Label3.TabIndex = 6
@@ -121,7 +120,7 @@ Partial Class frm_Main
         'PictureBox5
         '
         Me.PictureBox5.Image = CType(resources.GetObject("PictureBox5.Image"), System.Drawing.Image)
-        Me.PictureBox5.Location = New System.Drawing.Point(38, 509)
+        Me.PictureBox5.Location = New System.Drawing.Point(38, 513)
         Me.PictureBox5.Name = "PictureBox5"
         Me.PictureBox5.Size = New System.Drawing.Size(64, 64)
         Me.PictureBox5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize
@@ -131,7 +130,7 @@ Partial Class frm_Main
         'Label4
         '
         Me.Label4.AutoSize = True
-        Me.Label4.Location = New System.Drawing.Point(196, 536)
+        Me.Label4.Location = New System.Drawing.Point(193, 537)
         Me.Label4.Name = "Label4"
         Me.Label4.Size = New System.Drawing.Size(136, 17)
         Me.Label4.TabIndex = 8
@@ -140,7 +139,7 @@ Partial Class frm_Main
         'PictureBox6
         '
         Me.PictureBox6.Image = CType(resources.GetObject("PictureBox6.Image"), System.Drawing.Image)
-        Me.PictureBox6.Location = New System.Drawing.Point(689, 400)
+        Me.PictureBox6.Location = New System.Drawing.Point(689, 383)
         Me.PictureBox6.Name = "PictureBox6"
         Me.PictureBox6.Size = New System.Drawing.Size(64, 64)
         Me.PictureBox6.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize
@@ -150,7 +149,7 @@ Partial Class frm_Main
         'Label5
         '
         Me.Label5.AutoSize = True
-        Me.Label5.Location = New System.Drawing.Point(808, 433)
+        Me.Label5.Location = New System.Drawing.Point(811, 407)
         Me.Label5.Name = "Label5"
         Me.Label5.Size = New System.Drawing.Size(85, 17)
         Me.Label5.TabIndex = 10
@@ -159,7 +158,7 @@ Partial Class frm_Main
         'Label6
         '
         Me.Label6.AutoSize = True
-        Me.Label6.Location = New System.Drawing.Point(811, 658)
+        Me.Label6.Location = New System.Drawing.Point(811, 655)
         Me.Label6.Name = "Label6"
         Me.Label6.Size = New System.Drawing.Size(103, 17)
         Me.Label6.TabIndex = 11
@@ -168,7 +167,7 @@ Partial Class frm_Main
         'PictureBox7
         '
         Me.PictureBox7.Image = CType(resources.GetObject("PictureBox7.Image"), System.Drawing.Image)
-        Me.PictureBox7.Location = New System.Drawing.Point(689, 288)
+        Me.PictureBox7.Location = New System.Drawing.Point(689, 269)
         Me.PictureBox7.Name = "PictureBox7"
         Me.PictureBox7.Size = New System.Drawing.Size(64, 64)
         Me.PictureBox7.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize
@@ -178,7 +177,7 @@ Partial Class frm_Main
         'Label7
         '
         Me.Label7.AutoSize = True
-        Me.Label7.Location = New System.Drawing.Point(814, 316)
+        Me.Label7.Location = New System.Drawing.Point(811, 293)
         Me.Label7.Name = "Label7"
         Me.Label7.Size = New System.Drawing.Size(123, 17)
         Me.Label7.TabIndex = 13
@@ -187,26 +186,17 @@ Partial Class frm_Main
         'PictureBox8
         '
         Me.PictureBox8.Image = CType(resources.GetObject("PictureBox8.Image"), System.Drawing.Image)
-        Me.PictureBox8.Location = New System.Drawing.Point(689, 509)
+        Me.PictureBox8.Location = New System.Drawing.Point(689, 513)
         Me.PictureBox8.Name = "PictureBox8"
         Me.PictureBox8.Size = New System.Drawing.Size(64, 64)
         Me.PictureBox8.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize
         Me.PictureBox8.TabIndex = 14
         Me.PictureBox8.TabStop = False
         '
-        'GroupBox1
-        '
-        Me.GroupBox1.Location = New System.Drawing.Point(689, 108)
-        Me.GroupBox1.Name = "GroupBox1"
-        Me.GroupBox1.Size = New System.Drawing.Size(243, 134)
-        Me.GroupBox1.TabIndex = 15
-        Me.GroupBox1.TabStop = False
-        Me.GroupBox1.Text = "Current Cohort Statistics:"
-        '
         'Label8
         '
         Me.Label8.AutoSize = True
-        Me.Label8.Location = New System.Drawing.Point(811, 535)
+        Me.Label8.Location = New System.Drawing.Point(811, 537)
         Me.Label8.Name = "Label8"
         Me.Label8.Size = New System.Drawing.Size(121, 17)
         Me.Label8.TabIndex = 16
@@ -219,7 +209,6 @@ Partial Class frm_Main
         Me.BackColor = System.Drawing.SystemColors.Window
         Me.ClientSize = New System.Drawing.Size(1006, 721)
         Me.Controls.Add(Me.Label8)
-        Me.Controls.Add(Me.GroupBox1)
         Me.Controls.Add(Me.PictureBox8)
         Me.Controls.Add(Me.Label7)
         Me.Controls.Add(Me.PictureBox7)
@@ -268,6 +257,5 @@ Partial Class frm_Main
     Friend WithEvents PictureBox7 As PictureBox
     Friend WithEvents Label7 As Label
     Friend WithEvents PictureBox8 As PictureBox
-    Friend WithEvents GroupBox1 As GroupBox
     Friend WithEvents Label8 As Label
 End Class
