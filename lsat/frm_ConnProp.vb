f410ffec07804fbbb2079273de48f97eac972517
Jayden Wheeler <jwheeler@larkinstreetyouth.org>
2018-12-26 11:43:42 -0800

    Modified connection properties dialog

--- lsat/frm_ConnProp.vb
+++ lsat/frm_ConnProp.vb
@@ -7,7 +7,40 @@
         Me.Hide()
     End Sub
 
-    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles txtb_dbUser.TextChanged
+    Private Sub txtb_dbUser_TextChanged(sender As Object, e As EventArgs) Handles txtb_dbUser.TextChanged
+        ' Save the text entered in the dbUser text box into the user settings.
+        '
+        My.Settings.dbUser = txtb_dbUser.Text
+    End Sub
 
+    Private Sub txtb_dbPass_TextChanged(sender As Object, e As EventArgs) Handles txtb_dbPass.TextChanged
+        ' Save the text entered in the dbPass text box into the user settings.
+        '
+        My.Settings.dbPass = txtb_dbPass.Text
+    End Sub
+
+    Private Sub txtb_dbName_TextChanged(sender As Object, e As EventArgs) Handles txtb_dbName.TextChanged
+        ' Save the text entered in the dbName text box into the user settings.
+        '
+        My.Settings.dbName = txtb_dbName.Text
+    End Sub
+
+    Private Sub txtb_dbHost_TextChanged(sender As Object, e As EventArgs) Handles txtb_dbHost.TextChanged
+        ' Save the text entered in the dbHost text box into the user settings.
+        '
+        My.Settings.dbHost = txtb_dbHost.Text
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        ' Write the new values to the system preferences object then close the form.
+        My.Settings.Save()
+        frm_Login.Show()
+        Me.Hide()
+    End Sub
+
+    Private Sub txtb_dbPort_TextChanged(sender As Object, e As EventArgs) Handles txtb_dbPort.TextChanged
+        ' Save the text entered in the dbPort text box into the user settings.
+        '
+        My.Settings.dbPort = txtb_dbPort.Text
     End Sub
 End Class
